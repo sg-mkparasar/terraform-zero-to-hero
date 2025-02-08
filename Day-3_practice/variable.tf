5d0ef7da617fd4fb39ace185c0663cc35e09ea39
Mohit Parasar <mohit.parasar@swissgrid.ch>
2025-02-08 17:31:05 +0000

    day 3 practice without module

---
+++ Day-3_practice/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+    description = "AWS Ec2 instance image id"
+}
+
+variable "instance_type_value" {
+    description = "type of instance eligible for free tier"
+}
+
+variable "subnet_id_value" {
+    description = "subnet id where EC2 instance has to be created"
+  
+}
